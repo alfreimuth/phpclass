c3da18b7c410cab8d2810a4cca16a28f6f43a28c
Alex Freimuth <alexander.freimuth2005@gmail.com>
2025-01-15 21:56:32 -0600

    First Commit and Push

--- index.php
+++ index.php
@@ -6,8 +6,28 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Alexander's Webpage</title>
+    <link rel="stylesheet" type="text/css" href="css/base.css">
 </head>
 <body>
-<h1>Hello World!</h1>
+<div id="wrapper">
+    <header>
+        <h1>PHP Class - 1/22/2025</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Countdown</a></li>
+            <li><a href="#">Loops</a></li>
+            <li><a href="#">4</a></li>
+        </ul>
+    </nav>
+    <main>
+        <img src="img/notreal.jpg" width="300px" alt="totally me">
+        <p>Remind me to change picture</p>
+    </main>
+    <footer>
+        <p>&copy; 202X alexander.com</p>
+    </footer>
+</div>
 </body>
 </html>
